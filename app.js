ce1c8c1b407d76a4dd6604013eb23b25229e0387
Neags <38869150+Neags@users.noreply.github.com>
2022-03-08 15:28:50 -0800

    adding 11-5 got ahead on wrong tab earlier now section 5 is done

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 // import the data from data.js
 const tableData = data;
+
+function buildTable(data){
+    tbody.html("")
+    
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Reference the HTML table using d3
