dded600f543e2bec9fc0ce88e97e8fc72e989d8b
Neags <38869150+Neags@users.noreply.github.com>
2022-03-08 18:31:45 -0800

    Adding 11-5 done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const tableData = data;
 
 function buildTable(data){
     tbody.html("")
-    
+
     data.forEach((dataRow) => {
         let row = tbody.append("tr");
 
@@ -14,26 +14,19 @@ function buildTable(data){
     });
 }
 
+function handleClick(){
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
 
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
 
+    buildTable(filteredData);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+d3.selectAll("#filter-btn").on("click", handleClick)
 //Reference the HTML table using d3
+
+
+buildTable(tableData);
